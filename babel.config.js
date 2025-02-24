4f308417c5c1c1d556fb6795b7f82cfeb90ba714
thelllah <ganzaaimeedaniella@gmail.com>
2025-02-24 11:57:02 +0200

    initial commit

--- babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports= {
-    presets:['babel-preset-expo'],
-    plugins: [
-        'expo-router/babel', 
-        'nativewind/babel' // Only if you're using Tailwind (NativeWind)
-      ],
-}
