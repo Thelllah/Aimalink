ea9c2e01c796d535316630761f2870b47f9bfed8
Ganza Aimee Daniella <daniellaganza30@gmail.com>
2025-03-03 14:15:58 +0200

    schedule an appointmnet

--- app/components/ScheduleApp.tsx
+++ app/components/ScheduleApp.tsx
@@ -1,50 +1,62 @@
+import { useRouter } from 'expo-router';
 import React from 'react';
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Image, SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 function ScheduleApp() {
-  return (
-    <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-        <Text style={styles.title}>Don't waste time</Text>
-        <Text style={styles.schedule}>Schedule a Donation</Text>
-        </View>
-        <View style={styles.hospitalCont}>
-
-        </View>
-    </SafeAreaView>
-  );
+    const router = useRouter()
+    return (
+        <SafeAreaView style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>Don't waste time</Text>
+                <Text style={styles.schedule}>Schedule a Donation</Text>
+            </View>
+            <View style={styles.hospitalCont}>
+                <Image source={require('@/assets/images/doctor.png')} />
+            </View>
+            <TouchableOpacity style={styles.btn} onPress={() => { router.push('/components/Terms') }}>
+                <Text style={{ color: "white", fontFamily: "Poppins", padding: 10 }}>Schedule an appointment</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
-const styles= StyleSheet.create({
-    container:{
+const styles = StyleSheet.create({
+    container: {
         flex: 1,
         backgroundColor: "#fff",
-        alignItems:"center"
     },
-    header:{
-        flexDirection: "row",
-        alignItems: "center",
-        gap: 20,
-        marginTop: 10,
+    header: {
+        marginTop: 25,
         paddingHorizontal: 10,
     },
-    title:{
+    title: {
         color: "#717171",
         paddingHorizontal: 10,
         marginVertical: 10,
         fontFamily: "Nunito",
         fontSize: 15,
-        alignSelf:"flex-start"
+        alignSelf: "flex-start"
     },
-    schedule:{
-        fontFamily:"Poppins",
-        color:"#2E2E30",
+    schedule: {
+        fontFamily: "Poppins",
+        color: "#2E2E30",
+        fontSize: 20,
+        paddingHorizontal: 10
     },
-    hospitalCont:{
+    hospitalCont: {
         paddingHorizontal: 10,
-        alignSelf:"flex-start",
-        backgroundColor:"#FFB6B6",
-        
-    }
+        alignSelf: "flex-start",
+    },
+    btn: {
+        width: "80%",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#F05E5E",
+        borderRadius: 32,
+        marginTop: 10,
+        padding: 5,
+        marginBottom: 20,
+        alignSelf: "center",
+    },
 })
 
 export default ScheduleApp;
